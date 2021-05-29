42d97ee9e97a35823e5dfac2751706f3990533bf
寺下大典 <terashitadaisuke@terashitaMacBookPro.local>
2021-05-29 22:35:00 +0900

    記事投稿処理の追加

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -4,13 +4,33 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Article;
+use App\Http\Requests\ArticleRequest;
 
 class ArticleController extends Controller
 {
+    //一覧画面表示
     public function index()
     {
         $articles = Article::all()->sortByDesc('created_at');
         
         return view('articles.index', ['articles' => $articles]);
+    }
+
+    //記事投稿画面を表示
+    public function create(){
+        return view('articles.create');
+    }
+
+    //記事投稿処理
+    public function store(ArticleRequest $request){
+        $article = new Article();
+
+        // $article->title = $request->title;
+        // $article->body = $request->body;
+        $article->fill($request->all());
+        $article->user_id = $request->user()->id;
+        $article->save();
+
+        return redirect()->route('articles.index');
     }
 }
