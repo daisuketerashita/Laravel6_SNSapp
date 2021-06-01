8d400b8e4049b2d6c45053ff3257f71b478a1a93
daisuketerashita <ekusiad.tera@icloud.com>
2021-06-01 21:41:12 +0900

    isLikedByメソッドの作成

--- app/Article.php
+++ app/Article.php
@@ -4,6 +4,7 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Article extends Model
 {
@@ -15,6 +16,18 @@ class Article extends Model
     //Userモデルへのリレーション
     public function user(): BelongsTo{
         return $this->belongsTo('App\User');
+    }
+
+    //likesを中間テーブルとするリレーション
+    public function likes(): BelongsToMany{
+        return $this->belongsToMany('App\User','likes')->withTimestamps();
+    }
+
+    //ユーザーモデルを渡すとそのユーザーがこの記事をいいね済みかどうかを返すメソッド
+    public function isLikedBy(?User $user): bool{
+        return $user
+            ? (bool)$this->likes->where('id',$user->id)->count()
+            : false;
     }
 
 }
